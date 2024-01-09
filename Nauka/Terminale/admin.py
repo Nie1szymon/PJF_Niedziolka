c194ab020fdad10353bf5d98a92c792442b119e0
Nie1szymon <154265467+Nie1szymon@users.noreply.github.com>
2024-01-09 23:23:58 +0100

    Delete Nauka directory

--- Nauka/Terminale/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Terminale, History
-
-# Register your models here.
-admin.site.register(Terminale)
-admin.site.register(History)
--- Nauka/Terminale/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FunkcjeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Terminale'
